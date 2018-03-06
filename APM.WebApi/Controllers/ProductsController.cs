38cae922518d986c835b505f5e962a829e6d9ef0
100vision <30432527+100vision@users.noreply.github.com>
2018-03-06 22:06:13 +0800

    添加项目文件。

---
+++ APM.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using APM.WebApi.Models;
+using APM.WebApi.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace APM.WebApi.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        // GET: api/Products
+        public IEnumerable<Product> Get()
+        {
+            var productRepo = new ProductRepo();
+            return productRepo.Retrieve();
+
+        }
+
+        // GET: api/Products/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Products
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Products/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Products/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
